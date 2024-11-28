84c903c6d023975fe1e2f222d40b0739f824a2e7
nirabh567 <nirabhshukla567@gmail.com>
2024-11-28 09:39:22 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple form validation
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+    alert('Thank you for your message! We will get back to you soon.');
+});
